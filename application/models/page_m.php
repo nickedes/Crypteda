dc6b0e48f6067fe59e3e847a2f886a01dffd1f87
Zishan Ahmad <zishanahmad@outlook.com>
2014-08-14 23:25:22 +0530

    Codeigniter files

---
+++ application/models/page_m.php
@@ -0,0 +1,8 @@
+<?php
+class Page_m extends MY_Model {
+	protected $_table_name = 'pages';
+	protected $_primary_key = 'id';
+	protected $_primary_filter = 'intval';
+	protected $_order_by = 'order';
+	protected $_rules = array();
+}
---
+++ application/views/landing_layout.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jamia Connect</title>
+	<style type="text/css">
+		body {
+	      text-align: center;
+	      /*background: url("http://dash.ga.co/assets/anna-bg.png");*/
+	      background: url("/jmiconnect/assets/images/bg.png");
+	      background-size: cover;
+	      background-position: center;
+	      color: white;
+	      font-family: helvetica;
+    	}
+    	p {
+    		font-size: 20px;
+    	}
+    	input {
+    		border: 0;
+    		padding: 10px;
+    		margin-left: 5px;
+    	}
+    	input[type="submit"] {
+    		background: red;
+    		color: white;
+    	}
+	</style>
+</head>
+<body>
+	<h1>Welcome to Jamia Connect</h1>
+	<p>A place to get in touch, collaborate and share with JMI'tes</p>
+	<form>
+		<input type="email" placeholder="Email" name="email">
+		<input type="password" placeholder="Password" name="password">
+		<input type="submit" name="submit" value="Login">
+	</form>
+</body>
+</html>
