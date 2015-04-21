af0ad18abd7e8a66c79b45817980d9224e83340f
Shadab Zafar <dufferzafar0@gmail.com>
2015-04-21 20:36:09 +0530

    Refactored code a bit

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,4 @@
+from tables import *
 
 def substitute(s, sbox):
     substi = []
@@ -27,16 +28,20 @@ def gen(key):
         for j in range(0, len(first_key[i]), 2):
             join_by_2.append(''.join(first_key[i][j:j + 2]))
         first_key[i] = join_by_2
+
     w = []
     for i in range(44):
         w.append([])
+
     # 4 words
     for i in range(len(first_key)):
         for j in range(len(first_key[i])):
             w[j].append(first_key[i][j])
         keys[i] = w[i]
+
     # no. of words
     i = 4
+
     # until no. of words = 44
     while i < 44:
         temp = []
@@ -70,10 +75,6 @@ def sub(word):
 
 def shiftrows(s):
     shifted_s = []
-
-    # for k in range(0, int(len(s) / 4)):
-    # shifted_s.append([])
-
     for i in range(len(s)):
         shifted_s.append(s[i][i:] + s[i][:i])
 
@@ -143,62 +144,31 @@ def add_round_key(state, key):
 s = [['f3', 'cf', '4d', '15'], ['bf', '4b', 'fb', 'a6'],
      ['8b', 'e2', '52', '58'], ['12', 'ab', '4b', 'b8']]
 
-# key = 'ffffffffffffffffffffffffffffffff'
 key = ['d', '6', 'd', '3', 'a', '3', '9', '7', '6', '4', 'e', '8', '9', '2',
        '0', '4', '0', '8', '2', 'e', 'b', '5', 'd', 'd', '2', 'b', '8', 'e',
        'd', '5', '7', '6']
 
-sbox = [['63', '7c', '77', '7b', 'f2', '6b', '6f', 'c5',
-         '30', '01', '67', '2b', 'fe', 'd7', 'ab', '76'],
-        ['ca', '82', 'c9', '7d', 'fa', '59', '47', 'f0',
-         'ad', 'd4', 'a2', 'af', '9c', 'a4', '72', 'c0'],
-        ['b7', 'fd', '93', '26', '36', '3f', 'f7', 'cc',
-         '34', 'a5', 'e5', 'f1', '71', 'd8', '31', '15'],
-        ['04', 'c7', '23', 'c3', '18', '96', '05', '9a',
-         '07', '12', '80', 'e2', 'eb', '27', 'b2', '75'],
-        ['09', '83', '2c', '1a', '1b', '6e', '5a', 'a0',
-         '52', '3b', 'd6', 'b3', '29', 'e3', '2f', '84'],
-        ['53', 'd1', '00', 'ed', '20', 'fc', 'b1', '5b',
-         '6a', 'cb', 'be', '39', '4a', '4c', '58', 'cf'],
-        ['d0', 'ef', 'aa', 'fb', '43', '4d', '33', '85',
-         '45', 'f9', '02', '7f', '50', '3c', '9f', 'a8'],
-        ['51', 'a3', '40', '8f', '92', '9d', '38', 'f5',
-         'bc', 'b6', 'da', '21', '10', 'ff', 'f3', 'd2'],
-        ['cd', '0c', '13', 'ec', '5f', '97', '44', '17',
-         'c4', 'a7', '7e', '3d', '64', '5d', '19', '73'],
-        ['60', '81', '4f', 'dc', '22', '2a', '90', '88',
-         '46', 'ee', 'b8', '14', 'de', '5e', '0b', 'db'],
-        ['e0', '32', '3a', '0a', '49', '06', '24', '5c',
-         'c2', 'd3', 'ac', '62', '91', '95', 'e4', '79'],
-        ['e7', 'c8', '37', '6d', '8d', 'd5', '4e', 'a9',
-         '6c', '56', 'f4', 'ea', '65', '7a', 'ae', '08'],
-        ['ba', '78', '25', '2e', '1c', 'a6', 'b4', 'c6',
-         'e8', 'dd', '74', '1f', '4b', 'bd', '8b', '8a'],
-        ['70', '3e', 'b5', '66', '48', '03', 'f6', '0e',
-         '61', '35', '57', 'b9', '86', 'c1', '1d', '9e'],
-        ['e1', 'f8', '98', '11', '69', 'd9', '8e', '94',
-         '9b', '1e', '87', 'e9', 'ce', '55', '28', 'df'],
-        ['8c', 'a1', '89', '0d', 'bf', 'e6', '42', '68',
-         '41', '99', '2d', '0f', 'b0', '54', 'bb', '16']]
-
 print("Plain text:", s)
+print()
+
 all_key = gen(key)
 key = word_to_key([all_key[0], all_key[1], all_key[2], all_key[3]])
 key_no = 4
 s = add_round_key(s, key)
-print(s)
-#   Round 1
+print("Round 1", s)
+
 round = 2
 while round < 11:
     round_text = mixcolumns(shiftrows(substitute(s, sbox)))
-    key = (word_to_key(
-        [all_key[key_no], all_key[key_no + 1], all_key[key_no + 2], all_key[key_no + 3]]))
+    key = (word_to_key([all_key[key_no], all_key[key_no + 1], all_key[key_no + 2], all_key[key_no + 3]]))
     s = add_round_key(round_text, key)
-    print(s)
+    print("Round %d" % round, s)
+
     key_no += 4
     round += 1
+
 last_round = shiftrows(substitute(s, sbox))
-last_key = (word_to_key(
-    [all_key[key_no], all_key[key_no + 1], all_key[key_no + 2], all_key[key_no + 3]]))
+last_key = (word_to_key([all_key[key_no], all_key[key_no + 1], all_key[key_no + 2], all_key[key_no + 3]]))
 encrypted_text = add_round_key(last_round, last_key)
-print("encrypted text: ", encrypted_text)
+print()
+print("Round 11 (Encrypted Text): ", encrypted_text)
