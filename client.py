41f896b7efc438fb7cd99fb6b01bead48da1d557
nikhil <nkmittal4994@gmail.com>
2015-05-03 16:12:17 +0530

    server sends encrypted msgs

--- client.py
+++ client.py
@@ -3,10 +3,13 @@ from decrypt import rmpad, to_str, decrypt
 
 import socket               # Import socket module
 
+key = 'Nikhil Mittal'
+
 s = socket.socket()         # Create a socket object
 host = socket.gethostname()  # Get local machine name
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
 
-print s.recv(1024)
+cipher_text = s.recv(1024)
+print(x)
--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 from encrypt import pad, to_hex, encrypt
-from decrypt import rmpad, to_str, decrypt
-
+from decrypt import to_str
 import socket               # Import socket module
+
+
+def encryption(plain, key):
+    inp = to_hex(pad(plain))
+    key = to_hex(pad(key))[0]
+
+    ct = []
+    for block in inp:
+        ct.append(encrypt(block, key))
+
+    encrypted_text = ""
+    for block in ct:
+        encrypted_text += to_str(block)
+    return encrypted_text
+
+key = 'Nikhil Mittal'
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname()  # Get local machine name
@@ -12,5 +27,6 @@ s.listen(5)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
     print 'Got connection from', addr
-    msg = raw_input("enter plaintext:")
-    c.send(msg)
+    msg = raw_input("enter message:")
+    encrypted_text = encryption(msg, key)
+    c.send(encrypted_text)
